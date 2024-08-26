a2c064dfed59e2c19ea3781d6dadec8e98b4c6c6
PranishKadel <pranishkadel1@gmail.com>
2024-08-26 08:39:27 +0545

    shifting mongo to online server

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); 
 //definfing the mongodb connection URL
-const link = "mongodb://127.0.0.1:27017/details";
-
+// const link = "mongodb://127.0.0.1:27017/details";
 //setting up mongodb connection
-mongoose.connect(link);
-
+// const mongo_url = 'mongodb+srv://pranishkadel1:pranishisgood@cluster0.04wh6.mongodb.net/'
+mongoose.connect(mongo_url);
 //get the defualt connection
 //Mongoose maintains a default connection object representing the mongodb connection
+const mongo_url = process.env.DB_URL;
+// const mongo_url = process.env.DB_URL_LOCAL;
 const db = mongoose.connection;
+
+
 
 //defining event listeners for database connection
 db.on("connected", () => {
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express(); //creating a app server
 const db = require("./db");
 const bodyParser = require("body-parser"); //
 app.use(bodyParser.json()); // req body
+require("dotenv").config(); // now the server knows that we are using all the configuration files using dotenv
 
 //import routes from the person routes
 
@@ -11,7 +12,9 @@ const foodroutes = require("./routes/food");
 
 app.use("/person", personRoutes); 
 app.use("/menu", foodroutes);
-app.listen(3000, () => {
+
+const PORT = process.env.PORT || 3000; // if in process.env.PORT there is port value that it uses the current value otherwise it will use the another value
+app.listen( PORT, () => {
   console.log("server is currently runnning");
 });
 
